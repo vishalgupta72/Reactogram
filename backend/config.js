952390bbae23b7b46885dba29d48e4073e58ebc0
Vishal Gupta <vishalnph@gmail.com>
2024-08-31 18:19:15 +0530

    online DB host

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,5 @@
 module.exports = {
-    MONGODB_URL: "mongodb://localhost:27017/reactogram",
-    JWT_SECRET: "sldknf2j3423on3nj4noi23joijo3i2j42"
+    // MONGODB_URL: "mongodb://localhost:27017/reactogram",
+    MONGODB_URL: process.env.MONGODB_URL,
+    JWT_SECRET: process.env.JWT_SECRET
 }
